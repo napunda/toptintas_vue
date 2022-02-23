e53d366889cca4dc2cf3ceb3c2fa2f05d90f11ef
napunda <andersondroide14@gmail.com>
2022-02-23 17:25:18 -0400

    version-3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -30,3 +31,14 @@
   color: #42b983;
 }
 </style>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
